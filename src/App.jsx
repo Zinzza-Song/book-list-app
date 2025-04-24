d8b58fd2ee936d0bdd7bb37dcb1fb311832f331d
Han-Bin Song <thdgksq@naver.com>
2025-04-24 11:51:08 +0900

    Footer 컴포넌트 생성 및 Theme 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
-import 'react'
+import { useState } from 'react'
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
-import SearchPage from './pages/SearchPage'
-import BookDetailPage from './pages/BookDetailPage'
-import { css, Global } from '@emotion/react'
+import { css, Global, ThemeProvider, useTheme } from '@emotion/react'
+import SearchPage from '@/pages/SearchPage'
+import BookDetailPage from '@/pages/BookDetailPage'
+import Footer from '@/components/Footer'
+import { themeDark, themeLight } from '@/libs/Theme'
 
 function App() {
-  const Layout = () => {
+  const [isDark, setIsDark] = useState(false)
+
+  const Layout = ({ isDark, setIsDark }) => {
+    const theme = useTheme()
+    console.log(theme)
     return (
       <div>
         <Global
           styles={css`
             body {
-              background-color: white;
-              color: black;
+              background-color: ${theme.background};
+              color: ${theme.text};
               transiton-duration: 0.2s;
               transiton-property: background-color, color;
             }
 
             a {
-              color: black;
+              color: ${theme.text};
               text-decoration: none;
             }
 
@@ -28,27 +34,45 @@ function App() {
             }
           `}
         />
-        <Outlet />
+
+        <div
+          css={css`
+            min-height: 90vh;
+          `}>
+          <Outlet />
+        </div>
+
+        <Footer
+          isDark={isDark}
+          setIsDark={setIsDark}
+        />
       </div>
     )
   }
 
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={<Layout />}>
+      <ThemeProvider theme={isDark ? themeDark : themeLight}>
+        <Routes>
           <Route
-            index
-            element={<SearchPage />}
-          />
-          <Route
-            path="/book/:bookId"
-            element={<BookDetailPage />}
-          />
-        </Route>
-      </Routes>
+            path="/"
+            element={
+              <Layout
+                isDark={isDark}
+                setIsDark={setIsDark}
+              />
+            }>
+            <Route
+              index
+              element={<SearchPage />}
+            />
+            <Route
+              path="/book/:bookId"
+              element={<BookDetailPage />}
+            />
+          </Route>
+        </Routes>
+      </ThemeProvider>
     </BrowserRouter>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+      babel: {
+        plugins: ['@emotion/babel-plugin']
+      }
+    })
+  ],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }]
   }

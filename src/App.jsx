a437446d5856542f78a1c49fbfb06905a003978f
Han-Bin Song <thdgksq@naver.com>
2025-04-24 11:21:27 +0900

    Global Style 적용

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,32 @@ import 'react'
 import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import SearchPage from './pages/SearchPage'
 import BookDetailPage from './pages/BookDetailPage'
+import { css, Global } from '@emotion/react'
 
 function App() {
   const Layout = () => {
     return (
       <div>
+        <Global
+          styles={css`
+            body {
+              background-color: white;
+              color: black;
+              transiton-duration: 0.2s;
+              transiton-property: background-color, color;
+            }
+
+            a {
+              color: black;
+              text-decoration: none;
+            }
+
+            ul {
+              list-style: none;
+              padding: 0;
+            }
+          `}
+        />
         <Outlet />
       </div>
     )

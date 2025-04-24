62b670143dc1c859b19332a98afb275b36e86c62
Han-Bin Song <thdgksq@naver.com>
2025-04-24 11:13:53 +0900

    React Router Dom 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
 import 'react'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
+import SearchPage from './pages/SearchPage'
+import BookDetailPage from './pages/BookDetailPage'
 
 function App() {
-  return <>app</>
+  const Layout = () => {
+    return (
+      <div>
+        <Outlet />
+      </div>
+    )
+  }
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Layout />}>
+          <Route
+            index
+            element={<SearchPage />}
+          />
+          <Route
+            path="/book/:bookId"
+            element={<BookDetailPage />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App

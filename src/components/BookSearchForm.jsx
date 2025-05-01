c3044ba853aaf6a9697306c38793eca1e9c5a6a7
Han-Bin Song <thdgksq@naver.com>
2025-05-01 12:27:48 +0900

    Search Page 생성

--- src/components/BookSearchForm.jsx
+++ src/components/BookSearchForm.jsx
@@ -1,7 +1,52 @@
+import newStyled from '@emotion/styled'
 import React from 'react'
 
+const Input = newStyled.input`
+  outline: 0;
+  padding: 0.6rem 1rem;
+  border: 1px solid rgba(34, 36, 38, 0.5);
+  border-radius: 3px;
+  min-width: 280px;
+
+  &:focus,
+  &:active {
+    border-color: #85b7d9;
+  }
+  
+  @media (max-width: 778px) {
+    margin-top: 10px
+  }
+`
+
+const Button = newStyled.button`
+  background-color: #fcfcfd;
+  color: #36395a;
+  border: 1px solid rgba(34, 36, 38, 0.5);
+  padding: 0.6rem 1.5rem;
+  margin-left: 15px;
+  border-radious: 3px;
+  cursor: pointer;
+
+  &:hover {
+    transform: translateY(-2px);
+  }
+`
+
+const Form = newStyled.form`
+
+`
+
 const BookSearchForm = () => {
-  return <div>BookSearchForm</div>
+  return (
+    <Form>
+      <Input
+        type="search"
+        placeholder="책 이름을 검색해주세요."
+      />
+
+      <Button type="submit">Search</Button>
+    </Form>
+  )
 }
 
 export default React.memo(BookSearchForm)
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,7 +1,47 @@
+import newStyled from '@emotion/styled'
+import BookSearchForm from '@/components/BookSearchForm'
 import React from 'react'
 
+const LogoText = newStyled.h3`
+  margin: 0;
+`
+
+const Container = newStyled.div`
+  max-width: 960px;
+  padding: 15px;
+  margin: 0 auto;
+`
+
+const Header = newStyled.header`
+  border-bottom: 1px solid gray;
+`
+
+const HeaderContainer = newStyled(Container)`
+  display: flex;
+  align-items: center;
+  @media (max-width: 778px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+`
+
+const HeaderSearchForm = newStyled.div`
+  margin-left: auto;
+`
+
 const SearchPage = () => {
-  return <div>SearchPage</div>
+  return (
+    <>
+      <Header>
+        <HeaderContainer>
+          <LogoText>Book List</LogoText>
+          <HeaderSearchForm>
+            <BookSearchForm />
+          </HeaderSearchForm>
+        </HeaderContainer>
+      </Header>
+    </>
+  )
 }
 
 export default SearchPage
